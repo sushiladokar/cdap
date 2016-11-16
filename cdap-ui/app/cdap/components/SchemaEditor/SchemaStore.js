08494906826f54f04a9d9ac4f3e6a432b7afbf52
Ajai <ajainarayanan@gmail.com>
2016-11-15 23:35:47 -0800

    WIP1

---
+++ cdap-ui/app/cdap/components/SchemaEditor/SchemaStore.js
@@ -0,0 +1,61 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import {combineReducers, createStore} from 'redux';
+const defaultAction = {
+  type: '',
+  payload: {}
+};
+
+const defaultState = [
+  {
+    name: '',
+    type: 'string'
+  }
+];
+
+const schema = (state = defaultState, action = defaultAction) => {
+  switch(action.type) {
+    case 'ADD-ROW':
+      return [
+        ...state.slice(0, action.payload.index),
+        defaultState,
+        ...state.slice(action.payload.index, state.length)
+      ];
+    default:
+      return state;
+  }
+};
+
+let createStoreInstance = () => {
+  return createStore(
+    combineReducers({
+      schema
+    }),
+    {
+      schema: [
+        {
+          name: 'something',
+          type: 'string'
+        }
+      ]
+    }
+  );
+};
+
+export {createStoreInstance};
+let SchemaStore =  createStoreInstance();
+export default SchemaStore;
---
+++ cdap-ui/app/cdap/components/SchemaEditor/index.js
@@ -0,0 +1,59 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+require('./SchemaEditor.less');
+
+import SchemaStore from 'components/SchemaEditor/SchemaStore';
+
+export default class SchemaEditor extends Component {
+  constructor(props) {
+    super(props);
+    let state = SchemaStore.getState();
+    this.state = {
+      rows: state.schema
+    };
+  }
+  render() {
+    return (
+      <Provider store={SchemaStore}>
+        <div className="schema-editor">
+          <div className="schema-header">
+            <div className="field-name">
+              Name
+            </div>
+            <div className="field-type">
+              Type
+            </div>
+            <div className="field-isnull">
+              Null
+            </div>
+          </div>
+          {
+            this.state
+                .rows
+                .map((row, index) => {
+                  return (
+                    <h4 key={index}> {row.name} </h4>
+                  );
+                })
+          }
+        </div>
+      </Provider>
+    );
+  }
+}
